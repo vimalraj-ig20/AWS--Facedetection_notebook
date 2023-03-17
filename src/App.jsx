5f7381b552dc8f0d3d7f2ec541f48fd3fcd31ce2
vimalraj-ig20 <vimalraj.ig20@bitsathy.ac.in>
2023-03-17 14:02:45 +0530

    AWS-update

--- src/App.jsx
+++ src/App.jsx
@@ -82,34 +82,22 @@ const App = () => {
       </div>
 
       <p className={`text-${age}px tracking-wide mx-4`}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere qui
-        adipisci quam. Eius explicabo architecto, quibusdam accusamus culpa
-        ducimus nemo voluptates iusto laboriosam nesciunt aperiam
-        necessitatibus! Facilis repellat, eum dolorum deserunt debitis nemo id
-        aut iure nihil culpa. Expedita libero nemo accusantium quasi unde,
-        obcaecati, impedit autem temporibus et sequi asperiores molestiae omnis
-        cupiditate praesentium ab nam laudantium, non distinctio. Nulla quidem
-        mollitia quasi provident? Pariatur, provident adipisci. Explicabo,
-        perspiciatis eligendi perferendis corrupti cumque magnam vel labore quia
-        aperiam rem eos tempora nemo ad ex consectetur? Aperiam vero aliquam,
-        temporibus, ipsum quasi quod facere iste beatae impedit unde excepturi
-        tempora ratione! Ex, harum laudantium culpa vel voluptatem voluptatum
-        nam quia cumque reprehenderit omnis hic, et veritatis? Consequatur
-        facilis, eveniet voluptatibus necessitatibus suscipit blanditiis
-        deserunt sint doloremque, reprehenderit corrupti error cumque ducimus
-        totam accusantium. Vitae voluptatem, eveniet deleniti ratione sit illum!
-        Sint laboriosam reiciendis reprehenderit neque iste mollitia quis magnam
-        accusamus praesentium dicta blanditiis quidem, repudiandae ipsa esse
-        voluptatem voluptatibus distinctio deserunt sit ipsam. Odio illum veniam
-        a consequatur? Commodi corporis in nisi perferendis sapiente saepe rem
-        nobis, tempora voluptates voluptatibus pariatur, totam vel sed, ducimus
-        modi veniam? Nihil autem molestias reprehenderit sequi nobis recusandae
-        aspernatur commodi nisi eveniet, optio consectetur aperiam quibusdam
-        dicta laborum officia! Dolorum architecto tempora unde dolores.
-        Veritatis, impedit modi nisi tenetur, porro laboriosam officiis
-        necessitatibus nihil cupiditate voluptatum maiores et neque consequuntur
-        quia culpa voluptate ullam sed! Illum beatae adipisci natus laboriosam
-        aspernatur quo ea incidunt quibusdam.
+        The list of companies housing their IT environments in AWS reads like a who’s who of the most successful global organizations: 
+
+Adobe uses AWS to provide multi-terabyte operating environments for its customers by integrating its system with AWS Cloud. Adobe can focus on deploying and operating its own software instead of trying to deploy and manage the infrastructure.
+Airbnb, the online vacation rental marketplace for property owners and travelers to connect, maintains a huge infrastructure in AWS, using nearly all the available services.
+Autodesk develops software for the engineering, design, and entertainment industries. Using services like Amazon RDS and Amazon S3, Autodesk can focus on developing its machine learning tools instead of spending that time on managing the infrastructure 
+America Online (AOL) has used AWS to economize, closing data centers, and decommissioning about 14,000 in-house and co-located servers. They’ve moved mission-critical workloads to the cloud, extended its global reach, and saved millions of dollars on energy resources.
+BitDefender is an internet security software firm and its portfolio of software includes antivirus and anti-spyware products. Using Amazon EC2, they’re running several hundred instances that handle about five terabytes of data. BitDefender also uses the  Elastic Load Balancer feature to load balance the connection coming in to those instances across availability zones, providing seamless global delivery of service.
+BMW uses AWS for its new connected-car application, collecting sensor data from BMW 7-series cars to give drivers dynamically updated map information.
+Canon's imaging products division benefits from faster deployment times, lower cost, and global reach by using AWS to deliver cloud-based services such as mobile print and office imaging products.
+The world's largest cable company and the United States’ leading provider of internet service, Comcast, uses AWS in a hybrid environment. Out of all the other cloud providers, Comcast chose AWS for its flexibility and scalable hybrid infrastructure.
+Docker is a company helping to redefine the way developers build, ship, and run applications making use of containers. The Amazon EC2 container service helps them do it.
+Although much of the European Space Agency’s work is done by satellites, some of the program's data storage and computing infrastructure is built on Amazon Web Services.
+The Guardian newspaper uses a wide range of AWS services to power an analytic dashboard used by editors to see how stories are trending in real time.
+The Financial Times is one of the world's leading business news organizations and they use Amazon Redshift to perform their analyses. In fact, Redshift performed their analyses so quickly, some thought it was malfunctioning. They were used to running queries overnight. The Times found that the results were correct, just much faster.
+General Electric (GE) is, at this moment, migrating more than 9,000 workloads - including 300 disparate ERP systems - to AWS while reducing its data center footprint from 34 to four by 2021.
+The list goes on: Howard Medical School, McDonald's, NASA, Kellogg's, and many more benefit from Amazon Web Services
       </p>
     </div>
   );
